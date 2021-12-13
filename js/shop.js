6953e93ff29c3c7725ca0287a977894711ef1785
ITAcademyAdmin <92028251+ITAcademyAdmin@users.noreply.github.com>
2021-12-13 10:47:52 +0100

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -61,20 +61,6 @@ var cartList = [];
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
 
-var subtotal = {
-    grocery: {
-        value: 0, 
-        discount: 0
-    },
-    beauty: {
-        value: 0, 
-        discount: 0
-    },
-    clothes: {
-        value: 0, 
-        discount: 0
-    },
-};
 var total = 0;
 
 // Exercise 1
@@ -89,26 +75,23 @@ function cleanCart() {
 }
 
 // Exercise 3
-function calculateSubtotals() {
-    // 1. Create a for loop on the "cartList" array 
-    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+function calculateTotal() {
+    // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
-function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
-}
-
-// Exercise 5
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 }
 
-// Exercise 6
+// Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
 }
+
+
+// ** Nivell II **
 
 // Exercise 7
 function addToCart(id) {
@@ -117,13 +100,13 @@ function addToCart(id) {
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
-// Exercise 9
+// Exercise 8
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
-// Exercise 10
+// Exercise 9
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
