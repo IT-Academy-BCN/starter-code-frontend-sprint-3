27443aae4bca4c90bbaa1530ebd65381a97ac100
vicenlu <bcnluisvi@gmail.com>
2022-02-28 15:58:22 +0100

    code improved

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,14 @@
+// Get the input fields
+var password = document.querySelector(".password");
+var phone = document.querySelector('.phone');
+var name = document.querySelector('.name');
+
+// Get the error elements
+var errorPassword = document.getElementById("errorPassword");
+var errorName = document.getElementById('errorName');  
+var errorPhone = document.getElementById('errorPhone');  
 
 // Exercise 6
 function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById('fEmail');
-
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById('errorEmail');  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ''){
-		error++;
-	}
-
-	if(fEmail.value == ''){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
-
+    // Validate fields entered by the user: name, phone, password, and email
 }
