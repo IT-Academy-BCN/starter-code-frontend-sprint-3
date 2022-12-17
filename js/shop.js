df66c97946280847ee3f3afd2cbda729f32ed2bf
Juan Sóto <juansheba97@gmail.com>
2022-12-17 11:39:42 +0000

    Exercici 1

--- js/shop.js
+++ js/shop.js
@@ -73,8 +73,17 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      // Si se encuentra el producto, añádelo al array cartList
+      cartList.push(products[i]);
+    }
+  }
+  
+  
+  
+  return cartList;
+
 }
 
 // Exercise 2
