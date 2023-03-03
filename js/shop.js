861facc10aad478596e4565e4aa96bcd35ce94f3
JuanMa <mitild82@gmail.com>
2023-03-03 11:05:01 +0100

    First Exercise
    - Moved "products" array to its own file (data.js)
    
    - Completed "buy(id)" function
    
    Note: In order to access the array from data.js I had to add the type="module" attribute to the index.html file. I believe, because of that I had to convert functions into modular functions by adding the line of code: "window.function = function"

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,9 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+// I exported the "products" array to its own file so I am importing it now.
+import products from './data.js'
+
+// make modular functions
+window.buy = buy
+
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
 
@@ -74,7 +15,14 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
+    for(let i = 0; i < products.length; i++) {
+        if(products[i].id === id){
     // 2. Add found product to the cartList array
+            cartList.push(products[i])
+        }
+    }
+    // Log results
+    // console.log(cartList)
 }
 
 // Exercise 2
