75524e53a1dd27fc2a277db1466e40037ecdb603
Daiana <dauipoma@gmail.com>
2023-03-14 18:30:21 +0100

    nivel 1

--- js/checkout.js
+++ js/checkout.js
@@ -3,26 +3,81 @@
 function validate() {
 	var error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	const fName = document.getElementById("fName");
+	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+	const fLastName = document.getElementById("fLastN");
+	const fPassword = document.getElementById("fPassword");
+	const fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	var errorEmail = document.getElementById("errorEmail");   
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
+	if(/^[a-zA-Z]{3,}[ ]?[a-zA-Z]*$/.test(fName.value)){
+		fName.classList.remove('is-invalid')
+		fName.classList.add('is-valid')
+	} else {
+		fName.classList.remove('is-valid')
+		fName.classList.add('is-invalid')
+		error++
+	}
+	
+	if(/^[a-zA-Z]{3,}[ ]?[a-zA-Z]*$/.test(fLastName.value) ){
+		fLastName.classList.remove('is-invalid')
+		fLastName.classList.add('is-valid')
+	} else {
+		fLastName.classList.remove('is-valid')
+		fLastName.classList.add('is-invalid')
+		error++
+	}
+	
+	if(/^[a-zA-Z0-9]{3,}[@][a-zA-Z]+[.][a-zA-Z]+$/.test(fEmail.value)){
+		fEmail.classList.remove('is-invalid')
+		fEmail.classList.add('is-valid')
+	} else {
+		fEmail.classList.remove('is-valid')
+		fEmail.classList.add('is-invalid')
+		error++
+	}
+	
+	if(/^[a-zA-Z]{3,}[ a-zA-Z0-9]*$/.test(fAddress.value)){
+		fAddress.classList.remove('is-invalid')
+		fAddress.classList.add('is-valid')
+	} else {
+		fAddress.classList.remove('is-valid')
+		fAddress.classList.add('is-invalid')
+		error++
+	}
+	
+	// La contrasenya ha d'incloure números i lletres
+	if(/^[a-zA-Z0-9]{3,}$/.test(fPassword.value)){
+		fPassword.classList.remove('is-invalid')
+		fPassword.classList.add('is-valid')
+	} else {
+		fPassword.classList.remove('is-valid')
+		fPassword.classList.add('is-invalid')
+		error++
+	}
+	
+	if(fPhone.value.match(/^\d{9}$/)){
+		fPhone.classList.remove('is-invalid')
+		fPhone.classList.add('is-valid')
+	} else {
+		fPhone.classList.remove('is-valid')
+		fPhone.classList.add('is-invalid')
+		error++
 	}
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
+	if(!error){
 		alert("OK");
 	}
-
 }
+
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	validate()
+})
--- js/shop.js
+++ js/shop.js
@@ -71,36 +71,82 @@ var cart = [];
 
 var total = 0;
 
+const modalList = document.querySelector('#cart_list')
+const modalTotalPrice = document.querySelector('#total_price')
+const counter = document.querySelector('#count_product')
+
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    const [productById] = products.filter(product => product.id === id)
+    cartList.push(productById)
+    counter.innerHTML = cartList.length
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.length = 0
+    modalList.innerHTML = ''
+    modalTotalPrice.innerHTML = `0`
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    const initialValue = 0;
+    const sumTotalPrice = cartList.reduce((accumulator, product) => accumulator + product.price, initialValue)
+
+    modalTotalPrice.innerHTML = sumTotalPrice
+    // return sumTotalPrice
 }
 
 // Exercise 4
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    cartList.forEach(product => {
+        const getProduct = cart.filter(prod => prod.id === product.id)
+        if(getProduct.length){
+            const [repeteadProduct] = getProduct
+            repeteadProduct.quantity++
+            repeteadProduct.subtotal = repeteadProduct.price * repeteadProduct.quantity
+        } else {
+            cart.push({...product, quantity: 1, subtotal: product.price, subtotalWithDiscount: product.price})            
+        }
+    })
 }
 
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    cart
+    .filter(product => (product.id === 1 && product.quantity >= 3)|| (product.id === 3 && product.quantity >= 10))
+    .forEach(productWithDiscount => {
+        if(productWithDiscount.id === 1) {
+            productWithDiscount.subtotalWithDiscount = productWithDiscount.quantity * 10
+        } else {
+            productWithDiscount.subtotalWithDiscount = productWithDiscount.quantity * (productWithDiscount.price * 0.67)
+        }
+    })
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    generateCart()
+    applyPromotionsCart()
+
+    modalList.innerHTML = ''
+    
+    cart.forEach(product => {
+        modalList.innerHTML += `<tr>
+        <th scope="row">${product.name}</th>
+        <td>$${product.price}</td>
+        <td>${product.quantity}</td>
+        <td>$${product.subtotalWithDiscount}</td>
+      </tr>`
+    })
 }
 
 
