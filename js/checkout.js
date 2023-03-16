7fdb767bb9e5b15425d539e64b50ae502f46e655
Giovanny Lopez <giolr14@gmail.com>
2023-03-16 18:41:28 +0100

    nivel 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,71 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+    var error = 0;
+    // Get the input fields
+    var fName = document.getElementById("fName");
+    var lName = document.getElementById("lName");
+    var phone = document.getElementById("phone");
+    var password = document.getElementById("password");
+    var fEmail = document.getElementById("fEmail");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+    // Get the error elements
+    var errorName = document.getElementById("errorName");
+    var errorLastName = document.getElementById("errorLastName");
+    var errorPhone = document.getElementById("errorPhone");
+    var errorPassword = document.getElementById("errorPassword");
+    var errorEmail = document.getElementById("errorEmail");  
+    
+    // Validate fields entered by the user: name, phone, password, and email
+    if(fName.value.length < 3 || !/^[a-zA-Z]+$/.test(fName.value)){
+        error++;
+        fName.classList.add("is-invalid");
+        errorName.innerText = "El nombre debe tener al menos 3 caracteres y contener solo letras";
+    } else {
+        fName.classList.remove("is-invalid");
+        errorName.innerText = "";
+    }
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
+    if(lName.value.length < 3 || !/^[a-zA-Z]+$/.test(lName.value)){
+        error++;
+        lName.classList.add("is-invalid");
+        errorLastName.innerText = "Los apellidos deben tener al menos 3 caracteres y contener solo letras";
+    } else {
+        lName.classList.remove("is-invalid");
+        errorLastName.innerText = "";
+    }
+
+    if(phone.value.length !== 9 || isNaN(phone.value)){
+        error++;
+        phone.classList.add("is-invalid");
+        errorPhone.innerText = "El teléfono debe tener 9 dígitos y contener solo números";
+    } else {
+        phone.classList.remove("is-invalid");
+        errorPhone.innerText = "";
+    }
+     
+    if(password.value.length < 6 || !/\d/.test(password.value) || !/[a-zA-Z]/.test(password.value)){
+        error++;
+        password.classList.add("is-invalid");
+        errorPassword.innerText = "La contraseña debe tener al menos 6 caracteres, incluir números y letras";
+    } else {
+        password.classList.remove("is-invalid");
+        errorPassword.innerText = "";
+    }
+
+    if(fEmail.value.length === 0 || !/\S+@\S+\.\S+/.test(fEmail.value)){
+        error++;
+        fEmail.classList.add("is-invalid");
+        errorEmail.innerText = "El email debe tener un formato válido";
+    } else {
+        fEmail.classList.remove("is-invalid");
+        errorEmail.innerText = "";
+    }
+
+    if(error>0){
+        alert("Error");
+    }else{
+        alert("OK");
+    }
 
 }
--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,6 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
-   {
+    {
         id: 1,
         name: 'cooking oil',
         price: 10.5,
@@ -73,34 +73,126 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].id === id) {
+            cartList.push(products[i]);
+            console.log(cartList);
+            break;
+        }
+    }
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList = [];
+    console.log(cartList);
 }
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
+    var total = 0;
+  for (var i = 0; i < cartList.length; i++) {
+    total += cartList[i].price;
+  }
+  return total;
+  console.log("Total: " + total);
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
+ // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
+function generateCart() {
+    let cart = [];
+  for (let i = 0; i < cartList.length; i++) {
+    let item = cartList[i];
+    let existingItem = cart.find(elem => elem.id === item.id);
+    if (existingItem) {
+      existingItem.quantity++;
+      existingItem.subtotal = existingItem.price * existingItem.quantity;
+      if (existingItem.offer) {
+        let discount = Math.floor(existingItem.quantity / existingItem.offer.number) * existingItem.offer.percent * existingItem.price / 100;
+        existingItem.subtotalWithDiscount = existingItem.subtotal - discount;
+      } else {
+        existingItem.subtotalWithDiscount = existingItem.subtotal;
+      }
+    } else {
+      let newItem = {...item, quantity: 1, subtotal: item.price, subtotalWithDiscount: item.price};
+      cart.push(newItem);
+    }
+  }
+  return cart;
 }
+   
 
 // Exercise 5
-function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
-}
+    function applyPromotionsCart(cart) {
+        // Promoción botellas de aceite
+        for (let i = 0; i < cart.length; i++) {
+          const product = cart[i];
+          if (product.name === 'cooking oil' && product.cuantidad >= 3) {
+            const discountedPrice = 10;
+            product.subtotalWithDiscount = discountedPrice * product.cuantidad;
+          }
+        }
+      
+        // Promoción productos para hacer pastel
+        let totalCakeProducts = 0;
+        for (let i = 0; i < cart.length; i++) {
+          const product = cart[i];
+          if (product.name === 'Instant cupcake mixture') {
+            totalCakeProducts += product.cuantidad;
+          }
+        }
+        if (totalCakeProducts >= 10) {
+          const discountedPrice = 2/3;
+          for (let i = 0; i < cart.length; i++) {
+            const product = cart[i];
+            if (product.name === 'Instant cupcake mixture') {
+              product.subtotalWithDiscount = product.price * product.cuantidad * discountedPrice;
+            }
+          }
+        }
+      }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    const cartTable = document.querySelector('#cartModal .modal-body table');
+    const cartTotal = document.querySelector('#cartModal .modal-footer .total');
+  
+    // Clear the current cart table
+    cartTable.innerHTML = '';
+  
+    // Generate the table header row
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+      <th>Product</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th>Subtotal</th>
+      <th>Discount</th>
+      <th>Total</th>
+    `;
+    cartTable.appendChild(headerRow);
+  
+    // Generate a table row for each product in the cart
+    cart.forEach(product => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${product.name}</td>
+        <td>${product.price} €</td>
+        <td>${product.quantity}</td>
+        <td>${product.subtotal} €</td>
+        <td>${product.discount} €</td>
+        <td>${product.total} €</td>
+      `;
+      cartTable.appendChild(row);
+    });
+  
+    // Update the cart total
+    cartTotal.innerHTML = `Total: ${calculateTotal()} €`;
 }
 
 
@@ -119,7 +211,7 @@ function removeFromCart(id) {
     // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+    console.log("Open Modal");
+    printCart();
 }
