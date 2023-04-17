5136cdae20897dbd001dbe355ce3572782669ca5
Luiggi <luduve97@gmail.com>
2023-04-17 13:01:34 +0200

    finishing ex7

--- js/checkout.js
+++ js/checkout.js
@@ -11,60 +11,74 @@ function validate() {
 		// Quan l'usuari/ària introdueixi un camp que no compleixi les validacions anteriors, l'input s'ha de ressaltar en vermell i mostrar un missatge a la part inferior.
 
 		// Ajuda: podràs acolorir la vora de l'input en vermell i mostrar el missatge d'error manipulant el dom, encara que també pots usar la classe is-invalid de bootstrap.
-
+	
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
-	// const fAddress = document.getElementById('fAddress');
+	const fAddress = document.getElementById('fAddress');
 	const fLastName = document.getElementById('fLastN');
-	// const fPassword = document.getElementById('fPassword');
-	// const fPhone = document.getElementById('fPhone');
+	const fPassword = document.getElementById('fPassword');
+	const fPhone = document.getElementById('fPhone');
 
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");
-	// const errorAddress = document.getElementById('errorAddress');
+	const errorAddress = document.getElementById('errorAddress');
 	const errorLastName = document.getElementById('errorLastN');
-	// const errorPassword = document.getElementById('errorPassword');
-	// const errorPhone = document.getElementById('errorPhone');
+	const errorPassword = document.getElementById('errorPassword');
+	const errorPhone = document.getElementById('errorPhone');
 
 
+	let validate = false
 
-	let namePattern = /^[A-Z][a-z]{2,}$/;
-	let checkName = namePattern.test(fName);
+	const namePattern = /^[A-Z][a-z]{2,}$/;
+	let checkName = namePattern.test(fName.value);
+
+	const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	let checkEmail = emailPattern.test(fEmail);
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == "" && checkName == false){
+	if(fName.value == "" || checkName == false){
 		errorName.style.display = 'initial';
-		fName.classList.add("needs-validation");
-		console.log(checkName);
+		fName.classList.add("is-invalid");
 
 		error++;
 	} else {
-		console.log(checkName);
-		
+		fName.classList.remove("is-invalid");
+		fName.classList.add("is-valid")
 		errorName.style.display = 'none';
+
+		validate = true
 	}
 
-	if(fEmail.value == ""){
-		emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-		fEmail = emailPattern.test(fEmail);
+	if(fEmail.value == "" || checkEmail == false){
 		errorEmail.style.display = 'initial';
+		fEmail.classList.add("is-invalid");
 		error++;
+	} else {
+		fEmail.classList.remove("is-invalid");
+		fEmail.classList.add("is-valid");
+		errorEmail.style.display = 'none';
+
 	}
 
 	if (fAddress.value = "") {
 		errorAddress.style.display = 'initial';
+		fAddress.classList.add("is-invalid");
 		error++;
+	} else {
+		fAddress.classList.remove("is-invalid");
+		fAddress.classList.add("is-valid");
 	}
 	if (fLastName.value = "") {
-		let namePattern = /^[A-Z][a-z]{2,}$/;
-		fLastName = namePattern.test(fLastName);
-		console.log(fName);
+		fLastName.classList.add("is-invalid")
 		errorLastName.style.display = 'initial';
 		error++;
+	} else{
+		fLastName.classList.remove("is-invalid");
+		fLastName.classList.add
 	}
 	if (fPassword.value = "") {
 		errorPassword.style.display = 'initial';
