0923ea2e8595eff92f99115b537fc3f986e45368
Your Name <you@example.com>
2023-06-28 18:06:37 +0200

    Exercise 4

--- js/shop.js
+++ js/shop.js
@@ -107,7 +107,18 @@ function calculateTotal() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+    let foundItem;
+
+    cartList.forEach(item => {
+        foundItem = cart.find(product => product.id === item.id);
+        if (foundItem == undefined) {
+            item.quantity = 1;
+            cart.push(item);
+        } else {
+            foundItem.quantity++;
+        }
+    });
+}  
 
 // Exercise 5
 function applyPromotionsCart() {
