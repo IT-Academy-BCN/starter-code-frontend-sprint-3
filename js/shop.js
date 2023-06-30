ecbf97be8a28e6d5dd7317beedae767794e9d3fd
Your Name <you@example.com>
2023-06-30 11:51:21 +0200

    Exercise 6

--- js/shop.js
+++ js/shop.js
@@ -8,89 +8,128 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    let item;
+  // 1. Loop for to the array products to get the item to add to cart
+  let item;
 
-    for (let i in products) {
-        if (products[i].id == id) {
-            item = products[i];
-        }
+  for (let i in products) {
+    if (products[i].id == id) {
+      item = products[i];
     }
+  }
 
-    // 2. Add found product to the cartList array
-    cartList.push(item);
+  // 2. Add found product to the cartList array
+  cartList.push(item);
 }
 
 // Exercise 2
 function cleanCart() {
-    cartList.length = 0;
+  cartList.length = 0;
 }
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
-    let totalPrice = 0;
+  // Calculate total price of the cart using the "cartList" array
+  // Update: calculateTotal using array "cart" + quantities + added promotions
 
-    for (let i in cartList) {
-        totalPrice += cartList[i].price;
+  for (let i in cart) {
+    if (cart[i].subtotalWithDiscount != undefined) {
+      total += cart[i].subtotalWithDiscount;
+    } else {
+      total += cart[i].subtotal;
     }
-
-    return totalPrice;
+  }
 }
 
 // Exercise 4
 function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    let foundItem;
+  // Using the "cartlist" array that contains all the items in the shopping cart,
+  // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  let foundItem;
 
-    cartList.forEach(item => {
-        foundItem = cart.find(product => product.id === item.id);
-        if (foundItem == undefined) {
-            item.quantity = 1;
-            cart.push(item);
-        } else {
-            foundItem.quantity++;
-        }
-    });
-}  
+  cartList.forEach((item) => {
+    foundItem = cart.find((product) => product.id === item.id);
+    if (foundItem == undefined) {
+      item.quantity = 1;
+      cart.push(item);
+    } else {
+      foundItem.quantity++;
+    }
+  });
+}
 
 // Exercise 5
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
-    cart.forEach(item => {
-        item.subtotal = item.price * item.quantity;
-        if (item.offer != undefined) {
-            if (item.quantity >= item.offer.number) {
-                item.subtotalWithDiscount = Math.floor(item.subtotal - (item.subtotal * item.offer.percent / 100));
-            }
-        }
-        
-    })
+  // Apply promotions to each item in the array "cart"
+  cart.forEach((item) => {
+    item.subtotal = item.price * item.quantity;
+    if (item.offer != undefined) {
+      if (item.quantity >= item.offer.number) {
+        item.subtotalWithDiscount = Math.floor(
+          item.subtotal - (item.subtotal * item.offer.percent) / 100
+        );
+      }
+    }
+  });
 }
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  // Fill the shopping cart modal manipulating the shopping cart dom
+  generateCart();
+  applyPromotionsCart();
+  calculateTotal();
+  printItems();
+  printTotal();
 }
 
+function printItems() {
+  // Manipulate the <tbody> to add the items in array "cart"
+  let tableBody, tableRow, finalSubtotal;
+  tableBody = document.querySelector("tbody#cart_list");
+
+  tableBody.innerHTML = "";
+
+  cart.forEach((item) => {
+    tableRow = document.createElement("tr");
+
+    if (item.subtotalWithDiscount != undefined) {
+      finalSubtotal = item.subtotalWithDiscount;
+    } else {
+      finalSubtotal = item.subtotal;
+    }
+
+    tableRow.innerHTML = `
+            <th scope="row">${item.name}</th>
+            <td>$${item.price}</td>
+            <td>${item.quantity}</td>
+            <td>$${finalSubtotal}</td>
+        `;
+
+    tableBody.appendChild(tableRow);
+  });
+}
+
+function printTotal() {
+  // Display the total price of the items in array "cart"
+  document.getElementById("total_price").innerHTML = total;
+}
 
 // ** Nivell II **
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  // Refactor previous code in order to simplify it
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+  console.log("Open Modal");
+  printCart();
 }
